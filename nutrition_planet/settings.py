f352251abf727749eb5667e3f7b5e83f54b04dcc
llpw-build <llpw@hotmail.co.uk>
2026-07-24 19:04:00 +0100

    Create and set up apps

--- nutrition_planet/settings.py
+++ nutrition_planet/settings.py
@@ -37,7 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main',
+    'products',
+    'bag',
+    'checkout',
+    'profiles',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
