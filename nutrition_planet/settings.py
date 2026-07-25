fa40e8328397d442e68fa6b5fb3abb72d672c9c1
llpw-build <llpw@hotmail.co.uk>
2026-07-25 18:21:23 +0100

    Finish the setup up for Nutrition Planet

--- nutrition_planet/settings.py
+++ nutrition_planet/settings.py
@@ -11,19 +11,21 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+env = environ.Env()
+environ.Env.read_env(BASE_DIR / ".env")
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4lk)+flysw^&(=mo#ck!0i7iz0wu#(q=%2qnc73ezi*44$v(v-'
-
+SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool("DEBUG", default = True)
 
 ALLOWED_HOSTS = []
 
