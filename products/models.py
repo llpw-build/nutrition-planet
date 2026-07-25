9fd84c537ca8e2005ecd69e6ae2e350490884c73
llpw-build <llpw@hotmail.co.uk>
2026-07-25 22:14:14 +0100

    Add Brand model

--- products/models.py
+++ products/models.py
@@ -12,3 +12,29 @@ class Category(models.Model):
 
     def __str__(self):
         return self.friendly_name
+
+class Brand(models.Model):
+    slug = models.SlugField(
+        max_length=100,
+        unique=True,
+    )
+
+    name = models.CharField(
+        max_length=100,
+        unique=True,
+    )
+
+    description = models.TextField(
+        blank=True,
+    )
+
+    logo = models.ImageField(
+        upload_to="brands/logos/",
+    )
+
+    website = models.URLField(
+        blank=True,
+    )
+
+    def __str__(self):
+        return self.name
