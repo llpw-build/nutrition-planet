4bc77889b0ab3e8c5dcac3269de013cb00cb799e
llpw-build <llpw@hotmail.co.uk>
2026-07-26 10:34:55 +0100

    Add product model and register

--- products/models.py
+++ products/models.py
@@ -38,3 +38,60 @@ class Brand(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Product(models.Model):
+
+    name = models.CharField(
+        max_length=100,
+        unique=True,
+    )
+
+    brand = models.ForeignKey(
+        Brand,
+        on_delete=models.PROTECT,
+        related_name="products",
+    )
+
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.PROTECT,
+        related_name="products"
+    )
+
+    price = models.DecimalField(
+        max_digits=8,
+        decimal_places=2,
+    )
+
+    size_or_quantity = models.CharField(
+        max_length=100,
+    )
+
+    materials = models.TextField(
+        blank=True
+    )
+
+    ingredients = models.TextField(
+        blank=True
+    )
+
+    stock_quantity = models.PositiveIntegerField(
+        default=0
+    )
+
+    description = models.TextField(
+        blank=True,
+    )
+
+    image = models.ImageField(
+        upload_to="products/images/",
+    )
+
+    is_active= models.BooleanField(
+        default=True
+    )
+
+    def __str__(self):
+        return self.name
+
