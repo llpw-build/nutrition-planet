be3e5bcd996187e6f99310b90c7ada1a75ad4ed3
llpw-build <llpw@hotmail.co.uk>
2026-07-28 19:46:32 +0100

    Create first view, urls.py and template for products

--- products/views.py
+++ products/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+def all_products(request):
+
+    products = Product.objects.filter(
+        is_active=True
+    )
+
+    context = {
+        "products": products,
+    }
+
+    return render(
+        request,
+        "products/products.html",
+        context,
+    )
