b6595581851a8087d2da892be07e9a4679d25280
llpw-build <llpw@hotmail.co.uk>
2026-08-05 19:49:17 +0100

    Create url link from project to main app

--- nutrition_planet/urls.py
+++ nutrition_planet/urls.py
@@ -22,6 +22,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('products/', include("products.urls")),
+    path('', include("main.urls")),
 ]
 
 if settings.DEBUG:
