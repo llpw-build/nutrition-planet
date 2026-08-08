b003d7c9afecbbfc83e39497fdc0de1638cc582d
llpw-build <llpw@hotmail.co.uk>
2026-08-08 14:15:04 +0100

    implement automated tests to test product catalogue

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.urls import reverse
 
 # Create your tests here.
+
+class TestHomeView(TestCase):
+
+    def home_page_load_test(self):
+
+        response = self.client.get(reverse("home"))
+              
+        self.assertEqual(response.status_code, 200)
+
+        self.assertTemplateUsed(response, "main/index.html")
+
+        self.assertContains(response, "Planet Nutrition")
--- products/tests.py
+++ products/tests.py
@@ -18,6 +18,11 @@ class ProductTests(TestCase):
             friendly_name="Protein Powder",
         )
 
+        self.other_category = Category.objects.create(
+            name="creatine",
+            friendly_name="Creatine",
+        )
+
         self.product = Product.objects.create(
             name="Gold Standard Whey",
             brand=self.brand,
@@ -25,6 +30,23 @@ class ProductTests(TestCase):
             price="29.99",
             size_or_quantity="1 kg",
             image="products/images/gold-standard-whey.jpg",
+        )
+
+        self.other_product = Product.objects.create(
+            name="Creatine Monohydrate",
+            brand=self.brand,
+            category=self.other_category,
+            price="19.99",
+            size_or_quantity="500 g",
+        )
+
+        self.inactive_product = Product.objects.create(
+            name="Inactive Test Product",
+            brand=self.brand,
+            category=self.category,
+            price="9.99",
+            size_or_quantity="250 g",
+            is_active=False,
         )
 
     def test_str_returns_product_name(self):
@@ -45,14 +67,91 @@ class ProductTests(TestCase):
     def test_products_detail_page_returns_200(self):
         response = self.client.get(
             reverse(
-    "product_detail",
-    kwargs={
-        "product_id": self.product.id,
-    },
-))
+                "product_detail",
+                kwargs={
+                "product_id": self.product.id,
+            },
+        )
+    )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "products/product_detail.html")
+        self.assertContains(response, "Gold Standard Whey")
+
+    def test_search_returns_matching_products(self):
+        response = self.client.get(
+            reverse("all_products"),
+            {"q": "whey"},
+    )
+
+        self.assertEqual(response.status_code, 200)
+
+        self.assertContains(
+            response,
+            "Gold Standard Whey",
+        )
+
+        self.assertNotContains(
+        response,
+        "Creatine Monohydrate",
+    )
+
+    def test_category_filter_returns_matching_products(self):
+        response = self.client.get(
+            reverse("all_products"),
+            {"category": "protein-powder"},
+    )
+
+        self.assertContains(
+            response,
+            "Gold Standard Whey",
+    )
+
+        self.assertNotContains(
+            response,
+            "Creatine Monohydrate",
+        )
+
+    def test_products_sort_by_price_ascending(self):
+        response = self.client.get(
+            reverse("all_products"),
+            {
+             "sort": "price",
+             "direction": "asc",
+        },
+    )
+
+        products = list(response.context["products"])
+
+        self.assertEqual(
+            products,
+            [self.other_product, self.product],
+        )
+
+    def test_inactive_products_are_not_displayed(self):
+        response = self.client.get(reverse("all_products"))
+
+        self.assertContains(
+        response,
+        "Gold Standard Whey",
+        )
+
+        self.assertNotContains(
+        response,
+        "Inactive Test Product",
+        )
+
+    def test_invalid_product_id_returns_404(self):
+        response = self.client.get(
+            reverse(
+                "product_detail",
+                kwargs={
+                    "product_id": 999999,
+                },
+            )
+        )
 
         self.assertEqual(
             response.status_code,
-            200,
+            404,
         )
-
