50c626cf74d77f76a532a3051ca19804f1ea660f
llpw-build <llpw@hotmail.co.uk>
2026-08-08 15:31:43 +0100

    Add djangos built in login form and registration form

--- nutrition_planet/urls.py
+++ nutrition_planet/urls.py
@@ -22,6 +22,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('products/', include("products.urls")),
+    path('accounts/', include("django.contrib.auth.urls")),
     path('', include("main.urls")),
 ]
 
