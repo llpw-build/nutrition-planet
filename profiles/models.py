2e961078b7f2e7113819ca5b98db5c24a6b28b11
llpw-build <llpw@hotmail.co.uk>
2026-08-10 21:38:27 +0100

    Add userprofile and event signal

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.conf import settings 
 
 # Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="user_profile",
+    )
+
+    phone_number = models.CharField(
+        max_length=20,
+        blank=True,
+    )
+
+    address_line_1 = models.CharField(
+        max_length=200,
+        blank=True,
+    )
+
+    address_line_2 = models.CharField(
+        max_length=200,
+        blank=True,
+    )
+
+    town_or_city = models.CharField(
+        max_length=50,
+        blank=True,
+    )
+
+    postcode = models.CharField(
+        max_length=20,
+        blank=True,
+    )
+
+    country = models.CharField(
+        max_length=50,
+        blank=True,
+    )
