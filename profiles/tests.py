7be414a498bf9cb7824358d3f6a9b9af1ebff7a4
llpw-build <llpw@hotmail.co.uk>
2026-08-10 10:23:30 +0100

    implement tests for registration

--- profiles/tests.py
+++ profiles/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.urls import reverse
 
 # Create your tests here.
+class RegisterTests(TestCase):
+
+    def test_registration_page_returns_200(self):
+
+        response = self.client.get(reverse("register"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "registration/register.html",)
+        self.assertContains(response,"Register",)
