ce3e0fcc3fb3b7d761e557b7d6094b9ac7b20762
llpw-build <llpw@hotmail.co.uk>
2026-08-11 08:38:29 +0100

    Automated tests for UserProfile

--- profiles/tests.py
+++ profiles/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.urls import reverse
+from django.contrib.auth import get_user_model
 
 # Create your tests here.
 class RegisterTests(TestCase):
@@ -10,3 +11,31 @@ class RegisterTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "registration/register.html",)
         self.assertContains(response,"Register",)
+
+class UserProfileTests(TestCase):
+    def test_logged_in_user_can_access_profile(self):
+        User = get_user_model()
+
+        user = User.objects.create_user(
+            username="testuser",
+            password="testpass123",
+        )
+
+        self.client.login(
+            username="testuser",
+            password="testpass123",
+        )
+
+        response = self.client.get(
+            reverse("profile")
+        )
+
+    def test_anonymous_user_cannot_access_profile(self):
+        response = self.client.get(
+            reverse("profile")
+        )
+
+        self.assertEqual(
+            response.status_code,
+            302,
+        )
