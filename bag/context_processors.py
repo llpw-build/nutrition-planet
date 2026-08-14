3a89dced1a2524e1c75bfe1d0ee2f6b5a7db175b
llpw-build <llpw@hotmail.co.uk>
2026-08-14 09:55:23 +0100

    Add bag context processor polish

--- bag/context_processors.py
+++ bag/context_processors.py
@@ -1,7 +1,29 @@
 def bag_contents(request):
     bag = request.session.get("bag", {})
+
+    bag_items = []
     bag_count = sum(bag.values())
+    total = 0
+
+    for product_id, quantity in bag.items():
+        product = get_object_or_404(
+            Product,
+            id=product_id,
+        )
+
+        line_total = product.price * quantity
+        total += line_total
+
+        bag_items.append(
+            {
+                "product": product,
+                "quantity": quantity,
+                "line_total": line_total,
+            }
+        )
 
     return {
+        "bag_items": bag_items,
         "bag_count": bag_count,
+        "total": total,
     }
